f6f12dfb456f2719768a29894f739ae550f45ebb
neiljohnson <mr.neil.j@gmail.com>
2017-02-14 17:21:24 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// This program stores employee hours worked
+// and hourly pay rates in two parallel vectors
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    const int NUM_EMPS = 5;
+    vector<int> hours(NUM_EMPS);
+    vector<double> payRate(NUM_EMPS);
+    double grossPay;
+
+    // Get em
+}
